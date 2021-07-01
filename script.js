0583e6fc863f963973471df3f8bc0b2ca8ad7063
David <david-bitner@hotmail.com>
2021-07-01 10:52:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const div = document.querySelector("#txt");
+const div_title = document.querySelector("#txt-title");
+const div_txt = document.querySelector("#txt-txt");
+
+const arrow_left = document.querySelector("#btn-prev");
+const arrow_right = document.querySelector("#btn-next");
+
+const year = document.querySelector("#year");
+const today = new Date();
+
+// Copyright year
+year.innerHTML = today.getFullYear();
