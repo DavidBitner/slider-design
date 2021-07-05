a53af8913ac0fe3c75848064569efaa83b8f1bc1
David <david-bitner@hotmail.com>
2021-07-05 00:24:57 -0300

    Update script.js
    Animations functions

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const div = document.querySelector("#txt");
 const div_title = document.querySelector("#txt-title");
 const div_txt = document.querySelector("#txt-txt");
 
+const overlay1 = document.querySelector("#overlay1");
+const overlay2 = document.querySelector("#overlay2");
+
 const arrow_left = document.querySelector("#btn-prev");
 const arrow_right = document.querySelector("#btn-next");
 
@@ -31,16 +34,44 @@ function change_slide() {
   div.style.fontFamily = data[cur_slide].font;
 }
 
+// Disable/enable buttons
+function btn_status(disable = false) {
+  if (disable) {
+    arrow_left.style.pointerEvents = "none";
+    arrow_right.style.pointerEvents = "none";
+  } else {
+    setTimeout(() => {
+      arrow_left.style.pointerEvents = "auto";
+      arrow_right.style.pointerEvents = "auto";
+    }, 2000);
+  }
+}
+
 // Next slide function
 function next_slide() {
-  cur_slide < data.length - 1 ? cur_slide++ : (cur_slide = 0);
-  change_slide();
+  overlay1.style.animation = "fadein 2s";
+  btn_status(true);
+  setTimeout(() => {
+    cur_slide < data.length - 1 ? cur_slide++ : (cur_slide = 0);
+    overlay1.style.animation = "fadeout 2s";
+    change_slide();
+    reset_timer();
+    btn_status();
+  }, 2000);
 }
 
 // Prev slide function
 function prev_slide() {
-  cur_slide > 0 ? cur_slide-- : (cur_slide = data.length - 1);
-  change_slide();
+  overlay2.style.animation = "fadein 2s";
+  reset_timer();
+  btn_status(true);
+  setTimeout(() => {
+    cur_slide < data.length - 1 ? cur_slide++ : (cur_slide = 0);
+    overlay2.style.animation = "fadeout 2s";
+    change_slide();
+    reset_timer();
+    btn_status();
+  }, 2000);
 }
 
 // Timer that changes slide
@@ -63,7 +94,3 @@ arrow_left.addEventListener("click", function () {
   prev_slide();
   reset_timer();
 });
-
-// Animations
-// Next animation
-// Prev animation
